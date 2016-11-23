e2bfa02448b215e613df0e2239a43422aa1dcdea
lerchy <gagarinavp@gmail.com>
2016-11-23 09:51:39 +0300

    Function distance added to Main class

--- sandbox/src/main/java/ru/stqa/pft/homework/excercise2/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/homework/excercise2/Main.java
@@ -5,10 +5,15 @@ package ru.stqa.pft.homework.excercise2;
  */
 public class Main {
 
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+    }
 
     public static void main(String[] args){
-        Point a = new Point(0.0, 0.0);
-        Point b = new Point(0.0, 0.0);
-        System.out.println(a.distance(b));
+        Point p1 = new Point(2.0, 1.0);
+        Point p2 = new Point(6.0, 4.0);
+
+        System.out.println( "Distance calculated with the function in Main class: " + distance(p1, p2) );
+        System.out.println( "Distance calculated with the method in Point class: " + p1.distance(p2) );
     }
 }
