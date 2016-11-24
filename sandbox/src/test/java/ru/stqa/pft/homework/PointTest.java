b5b8f4ce2626973f8cf6f67431a0dde5e2e3228f
lerchy <gagarinavp@gmail.com>
2016-11-24 16:22:47 +0300

    two more assertions added to PointTest

--- sandbox/src/test/java/ru/stqa/pft/homework/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/homework/PointTest.java
@@ -13,5 +13,13 @@ public class PointTest {
         Point p1 = new Point(2.0, 1.0);
         Point p2 = new Point(6.0, 4.0);
         Assert.assertEquals(p1.distance(p2), 5.0);
+
+        Point p3 = new Point(-2.0, -1.0);
+        Point p4 = new Point(-6.0, -4.0);
+        Assert.assertEquals(p3.distance(p4), 5.0);
+
+        Point p5 = new Point(0, 0);
+        Point p6 = new Point(0, 0);
+        Assert.assertEquals(p5.distance(p6), 0.0);
     }
 }
