d64627408276a109d79fa0f182b0a065ec7ddeac
Valeriya Gagarina <gagarinavp@gmail.com>
2016-11-30 20:57:03 +0300

    insignificant intention changes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTest.java
@@ -24,9 +24,11 @@ public class UserCreationTest {
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys(password);
+
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
+
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
@@ -34,7 +36,9 @@ public class UserCreationTest {
     public void testUserCreation() {
 
         initializeUserCreation();
-        fillUserCreationForm(new UserData("Thomas", "A.", "Anderson", "Neo", "Software Engineer", "Matrix", "222-333-22", "333-222-33", "Lowertown, USA"));
+        fillUserCreationForm(new UserData("Thomas", "A.", "Anderson",
+                                          "Neo", "Software Engineer", "Matrix",
+                                          "222-333-22", "333-222-33", "Lowertown, USA"));
         submitUserCreation();
     }
 
