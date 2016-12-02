cc21c980eb5f4871af064f49826db10b650ca049
Valeriya Gagarina <gagarinavp@gmail.com>
2016-12-02 19:39:10 +0300

    GroupModificationTest added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by valeriyagagarina on 12/2/16.
+ */
+public class GroupModificationTest extends TestBase {
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("newTest1", "newTest2", "newTest3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
