47f49a488a44a52c3597900e7092800e47c7527f
Valeriya Gagarina <gagarinavp@gmail.com>
2016-12-19 18:21:40 +0300

    A little bit more of refactoring: BeforeMethod added to GroupModificationTest and GroupDelitionTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,19 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTest extends TestBase{
-    
-    @Test
-    public void testDeleteGroup() {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
         app.getNavigationHelper().goToGroupPage();
         if(!app.getGroupHelper().isThereGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+    
+    @Test
+    public void testDeleteGroup() {
         List<GroupData> before =  app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().deleteSelectedGroups();
